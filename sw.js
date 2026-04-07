f008fd1b7db0bad0ae1c7f698807d6c3d4710410
rootedlightwithin-create <rootedlightwithin@gmail.com>
2026-04-07 08:50:26 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const cacheName = "bubble-app-v1";
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
